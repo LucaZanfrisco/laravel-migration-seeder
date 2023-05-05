5d94d178fc613c13273cb41c642de4243d3a5154
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-05 17:05:06 +0200

    Model, controller and query filter

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Database\DBAL\TimestampType;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        
+        $trains = Train::where('departure_time', '>=', today())->get();
+
+        return view('home', compact('trains'));
+    }
+}
